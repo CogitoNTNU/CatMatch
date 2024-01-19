381dbace33858e712b82fb2a5df9ed6d864f13a2
Ulrik Røsby <ulrik@rosby.no>
2024-01-20 00:21:18 +0100

    feat: add recommendations endpoint

--- catmatch/server/endpoints.py
+++ catmatch/server/endpoints.py
@@ -1,8 +1,43 @@
-from fastapi import APIRouter
+import random
+
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+from catmatch.recsys.content_based import ContentBasedRecommender
+from catmatch.server.utils import (
+    IMAGE_PATHS,
+    convert_ratings_dict_to_array,
+    get_image_url_from_index,
+)
 
 recsys_router = APIRouter()
 
 
-@recsys_router.get("/hello")
-async def root():
-    return {"message": "Hello World"}
+class RecommendationsBody(BaseModel):
+    ratings: dict[str, bool | None]
+    number_of_recommendations: int = 20
+
+
+class RecommendationsResponse(BaseModel):
+    recommendations: list[str]
+
+
+recommender = ContentBasedRecommender(similarity_matrix_path="./similarity_matrix.hdf5")
+
+
+@recsys_router.post("/recommendations")
+async def recommendations(body: RecommendationsBody):
+    if all(value is None for value in body.ratings.values()):
+        # return random recommendations
+        random_recomms = random.sample(IMAGE_PATHS, body.number_of_recommendations)
+        return RecommendationsResponse(recommendations=random_recomms)
+
+    ratings_array = convert_ratings_dict_to_array(body.ratings)
+    if ratings_array is None:
+        raise HTTPException(status_code=400, detail="Invalid image URL")
+    # return random recommendations
+    recommendations = recommender.recommend_k_new_items(
+        ratings_array, body.number_of_recommendations
+    )
+    recommendation_urls = [get_image_url_from_index(index) for index in recommendations]
+    return RecommendationsResponse(recommendations=recommendation_urls)
